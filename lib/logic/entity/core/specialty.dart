83fd457faedd97dcbf04ed4d4768cfe0f4113363
linbonan03 <linbonan03@meituan.com>
2024-10-15 19:28:45 +0800

    ADD: 菜单页

--- lib/logic/entity/core/specialty.dart
+++ lib/logic/entity/core/specialty.dart
@@ -1,9 +1,5 @@
 import 'dart:collection';
 
-void main() {
-
-}
-
 class Dish {
   final int id;
   final String name;
@@ -12,7 +8,16 @@ class Dish {
   Recipe? recipe;
   List<DishRecord>? records;
 
-  Dish(this.id, this.name, this.showPic);
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'showPic': showPic,
+      'categoryId': categoryId
+    };
+  }
+
+  Dish(this.id, this.name, this.showPic, this.categoryId);
 }
 
 class Recipe {
@@ -27,14 +32,69 @@ class DishRecord {
   DishRecord(this.date);
 }
 
+class DishCategory {
+  int id;
+  String desc;
+  int ordinal;
+
+  DishCategory(this.id, this.desc, this.ordinal);
+}
+
 class Menu {
+  late Map<int, DishCategory> allCategories;
   Map<int, List<Dish>> catToDishes = HashMap();
+
+  Menu() {
+    _loadAllCategories();
+  }
 
   Map<int, List<Dish>> dishesGroupedByCategory() => catToDishes;
 
   void addToMenu({required Dish newDish}) {
     List<Dish>? dishes = catToDishes[newDish.categoryId];
-    dishes ??= List.empty(growable: true);
+    if (dishes == null) {
+      dishes = List.empty(growable: true);
+      catToDishes[newDish.categoryId] = dishes;
+    }
     dishes.add(newDish);
+  }
+
+  int categoryCount() {
+    int? catCount = catToDishes?.keys.length;
+    return catCount ?? 0;
+  }
+
+  DishCategory? existedCategoryOf(int index) {
+    if (catToDishes == null || index < 0) {
+      return null;
+    }
+
+    List<DishCategory> categories = List.empty(growable: true);
+    for (var catId in catToDishes.keys) {
+      DishCategory? category = allCategories[catId];
+      if (category != null) {
+        categories.add(category);
+      }
+    }
+    categories.sort((a, b) => a.ordinal.compareTo(b.ordinal));
+    return categories.elementAtOrNull(index);
+  }
+
+  List<Dish>? dishesOf(int categoryId) {
+    return catToDishes[categoryId];
+  }
+
+  void _loadAllCategories() {
+    //FIXME mock
+
+    List<DishCategory> categories = [
+      DishCategory(1, "肉类", 1),
+      DishCategory(2, "青菜", 2)
+    ];
+    allCategories = HashMap();
+    for (var category in categories) {
+      allCategories.update(category.id, (value) => category,
+          ifAbsent: () => category);
+    }
   }
 }
---
+++ lib/logic/entity/view/specialty_menu.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/components/list_tile/gf_list_tile.dart';
+import 'package:ktn4u/logic/entity/core/specialty.dart';
+import 'package:getwidget/getwidget.dart';
+
+class SpecialtyMenu extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SpecialtyMenuState();
+}
+
+class _SpecialtyMenuState extends State<SpecialtyMenu> {
+  late Menu menu;
+  late int _chosenCategoryId;
+
+  _refreshMenu() {
+    //FIXME mock
+    menu = Menu();
+    menu.addToMenu(newDish: Dish(1, 'test1', 'pci1', 1));
+    menu.addToMenu(newDish: Dish(2, 'test2', 'pci2', 1));
+    menu.addToMenu(newDish: Dish(3, 'test3', 'pci3', 2));
+  }
+
+  int _categoryCount() {
+    int? catCount = menu?.catToDishes?.keys.length;
+    if (catCount == null) {
+      return 0;
+    }
+    return catCount;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshMenu();
+    _chosenCategoryId = menu.existedCategoryOf(0)?.id ?? 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+            flex: 2,
+            child: ListView.builder(
+                itemCount: _categoryCount(),
+                itemBuilder: (context, index) {
+                  DishCategory? chosenCategory = menu.existedCategoryOf(index);
+                  return GFListTile(
+                    titleText: chosenCategory?.desc ?? '菜品',
+                    // icon: Icon(Icons.account_balance_wallet_rounded),
+                    onTap: () {
+                      setState(() {
+                        this._chosenCategoryId = chosenCategory?.id ?? 0;
+                      });
+                      print('tap');
+                    },
+                  );
+                })),
+        Expanded(
+            flex: 5,
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.topLeft,
+                  color: Colors.red,
+                  child: _generateDishList(_chosenCategoryId),
+                )
+              ],
+            ))
+      ],
+    );
+  }
+
+  Widget _generateDishList(int categoryId) {
+    List<Dish> dishes = menu.dishesOf(categoryId) ?? List.empty();
+    return Wrap(
+        spacing: 10.0,
+        direction: Axis.horizontal,
+        alignment: WrapAlignment.start,
+        children: List<Widget>.generate(
+                dishes.length, (int index) => _buildDishRow(dishes[index]))
+            .toList());
+  }
+
+  Widget _buildDishRow(Dish dish) {
+    return GFListTile(
+        color: Colors.white,
+        titleText: dish.name,
+        icon: Icon(Icons.play_circle));
+  }
+}
